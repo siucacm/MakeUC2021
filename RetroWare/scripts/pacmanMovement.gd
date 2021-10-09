6ca76f491aadd172e11d4c2cd0a4d36efd4fdc4e
ren-nay <naenae99rd@gmail.com>
2021-10-09 13:12:02 -0500

    Added pacman scene with moving pacman character

---
+++ RetroWare/scripts/pacmanMovement.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5
+
+var velocity = Vector2()
+var rotation_dir = 0
+
+func get_input():
+	velocity = Vector2()
+#	rotation_dir = 0
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		velocity.y = 0
+		rotation_dir = 0
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		velocity.y = 0
+		rotation_dir = PI
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		velocity.x = 0
+		rotation_dir = PI/2		
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		velocity.x = 0
+		rotation_dir = -PI/2
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	rotation = rotation_dir
+	velocity = move_and_slide(velocity)
