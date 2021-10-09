0b43864c48d9dbed80acc697222c1ca609734370
ren-nay <naenae99rd@gmail.com>
2021-10-09 15:54:42 -0500

    pacman can navigate the maze

--- RetroWare/scripts/pacmanMovement.gd
+++ RetroWare/scripts/pacmanMovement.gd
@@ -7,22 +7,22 @@ var velocity = Vector2()
 var rotation_dir = 0
 
 func get_input():
-	velocity = Vector2()
-#	rotation_dir = 0
+	# velocity = Vector2()
+	# rotation_dir = 0
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		velocity.x = 1
 		velocity.y = 0
 		rotation_dir = 0
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
+		velocity.x = -1
 		velocity.y = 0
 		rotation_dir = PI
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		velocity.y = 1
 		velocity.x = 0
 		rotation_dir = PI/2		
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.y = -1
 		velocity.x = 0
 		rotation_dir = -PI/2
 	velocity = velocity.normalized() * speed
